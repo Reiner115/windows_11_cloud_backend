cec963818ef68d879e606f0d0e3faef8a53121b1
MohamedTheDeveloper <mohamed.developer.2020@gmail.com>
2021-06-12 06:01:09 +0100

    changed pictures quality

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 console.log(`${process.env.HOST} ${process.env.USER} ${process.env.PASSWORD} ${process.env.DATABASE}`);
 const connection = mysql.createConnection({
-  host:  'localhost',
-  user:  'root',
-  password:   '',
-  database:  'food_app'
+  host:  'sql11.freemysqlhosting.net',
+  user:  'sql11418105',
+  password:   'hIMNsekdWx',
+  database:  'sql11418105'
 });
 connection.connect((err) => {
   if (err) throw err;
