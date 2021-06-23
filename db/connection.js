e1d143cef63ce622d5208c86f76d7bdb59b64966
MohamedTheDeveloper <mohamed.developer.2020@gmail.com>
2021-06-23 10:05:29 +0100

    connection

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 console.log(`${process.env.HOST} ${process.env.USER} ${process.env.PASSWORD} ${process.env.DATABASE}`);
 const connection = mysql.createConnection({
-	host : "sql11.freemysqlhosting.net",
-	database: "sql11419777",
-	user : "sql11419777",
-	password : "1LNqGJthlX",
+	host : "localhost",
+	database: "food_app",
+	user : "root",
+	password : "",
 	
   
 });
